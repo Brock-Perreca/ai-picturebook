aa0e0b56a21d803a9c41e6ba2397245cb770e39f
Brock <brock.perreca@gmail.com>
2023-05-22 02:46:51 -0600

    API Calls Fixed
    API text and image generations calls are working now.

--- index.js
+++ index.js
@@ -1,38 +1,159 @@
-const apiKey = 'KEY HERE';
+let model = "text-davinci-003";
+let apiKey;
+let inputElement;
+let outputElement;
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    inputElement = document.getElementById("prompt");
+    outputElement = document.getElementById("tempResponse");
+
+    //Stores key in localstorage
+    apiKeyElement = document.getElementById("key");
+    const OPENAI_API_KEY = "OPENAI_API_KEY";
+    apiKeyElement.addEventListener('change', (event) => {
+        let key = event.target.value;
+        localStorage.setItem(OPENAI_API_KEY, key);
+    });
+    if (localStorage.getItem(OPENAI_API_KEY)) {
+        apiKeyElement.value = localStorage.getItem(OPENAI_API_KEY);
+    }
+
+    setResponse("Hello, please type something");
+});
 
 function generatePictureBook() {
   const promptInput = document.getElementById("prompt");
-  //var apiKey = document.getElementById("key");
+  apiKey = document.getElementById("key").value;
   const promptText = promptInput.value.trim();
 
-  /*
   if (apiKey === "") {
     alert("Please enter an API Key.");
     return;
   }
-  */
+
   if (promptText === "") {
       alert("Please enter a prompt.");
       return;
   }
 
-  createPictureBook(promptText);
-
+  generateResponse(promptText);
 }
 
-/*
-async function generateText(promptText) {
-  // Replace this with your actual API call to GPT-3.5
-  var generatedText = `
-      Once upon a time, ${promptText}...
-      
-      This is the second paragraph of the story...
-      
-      This is the third paragraph of the story...
-  `;
-  return generatedText;
+function setResponse (sentence) {
+    if (sentence === "HTTP ERROR: 401") {
+        sentence += " — Your API Key has not been set properly.";
+    }
+    outputElement.innerText = sentence;
+    return;
 }
-*/
+
+async function generateResponse(promptText) {
+    setResponse("Waiting...");
+
+    if (promptText) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + apiKey
+                },
+                body: JSON.stringify({
+                    'model': model,
+                    'prompt': promptText,
+                    'temperature': 0,
+                    'max_tokens': 1000,
+                    'top_p': 1,
+                    'frequency_penalty': 1.2,
+                    'presence_penalty': 0
+                })
+            });
+
+            if (!response.ok) {
+                console.error("HTTP ERROR: " + response.status + "\n" + response.statusText);
+                setResponse("HTTP ERROR: " + response.status);
+            } else {
+                //Succesful API call!!
+                const data = await response.json();
+                let responseText = createResponse(data)
+                setResponse(responseText);
+
+                let img = document.getElementById("tempImageResponse");
+                setImage(responseText, img);
+            }
+        } catch (error) {
+            console.error("ERROR: " + error);
+        }
+    } else {
+        await setResponse("Please enter a prompt and select an engine");
+    }
+}
+
+function removePeriod(json) {
+    json.forEach(function (element, index) {
+        if (element === ".") {
+            json.splice(index, 1);
+        }
+    });
+    return json;
+}
+
+function createResponse(json) {
+    let response = "";
+    let choices = removePeriod(json.choices);
+    if (choices.length > 0) {
+        response = json.choices[0].text
+    }
+
+    return response;
+}
+
+
+
+async function setImage (imagePrompt, imgElement) {
+    const options = {
+        method: "POST",
+        headers: {
+            'Authorization': 'Bearer ' + apiKey,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: imagePrompt,
+            n: 1,
+            size: "1024x1024"
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        data?.data.forEach(imageObject => {
+            let imageSource = imageObject.url;
+            console.log (imageSource);
+            imgElement.src = imageSource;
+        })
+    } catch (error) {
+        console.error("THERE WAS AN ERROR: " + error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 async function generateText(promptText) {
   const requestOptions = {
@@ -66,7 +187,7 @@ async function generateImage(promptText) {
 }
 */
 
-async function generateImage(promptText) {
+async function generateImageOld(promptText) {
   const requestOptions = {
       method: 'POST',
       headers: {
@@ -88,8 +209,6 @@ async function generateImage(promptText) {
 
   return imageSrc;
 }
-
-
 
   // Call the API to generate the picture book (not
 
@@ -134,4 +253,3 @@ async function generateImage(promptText) {
         `;
     });
 }
-
