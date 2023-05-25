f7b6d65a1becb6557371af63930905f5305261f1
Brock <brock.perreca@gmail.com>
2023-05-25 04:42:50 -0600

    First functional draft of website
    Every basic feature has been added and should be functional

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ let model = "text-davinci-003";
 let apiKey;
 let inputElement;
 let outputElement;
+let updateZone;
+let storyPreface = "Create a 6 paragraph children's story book based on the following prompt: "
+let imagePromptPreface = "Visually desribe the following text to create a Dall-E image generation prompt: ";
+let dallEPreface = "cartoon ";
+let sampleText = `Once upon a time, there was a brave and adventurous monkey named Max. He lived in the jungle with his family and friends, but he always dreamed of exploring the world beyond. One day, Max decided to take a chance and set out on an adventure.
 
+Max traveled for days until he reached the shoreline of an island far away from home. As soon as he arrived, Max noticed something strange about this place - it was full of pirates! The pirates were everywhere - walking around town, sailing their ships in the harbor, and even flying flags from their masts!
+
+Max knew that these pirates were up to no good so he decided to investigate further. He followed them around town until they stopped at a large warehouse near the docks. Inside the warehouse were piles of treasure chests filled with gold coins!
+
+The monkeys heart raced as he realized what was happening - these pirates had stolen all this treasure from other ships! Without thinking twice about it, Max jumped into action and began fighting off all of the pirates one by one using his agility and strength!
+
+After what seemed like hours of battle against overwhelming odds, Max finally managed to defeat all of them single-handedly! With no more enemies left standing in his way, Max quickly grabbed as much treasure as possible before running back into the jungle with it safely tucked away under his arm.
+
+Back at home in his village deep within the jungle's depths ,Max shared some of his newfound wealth with everyone who had helped him along on this journey . Everyone celebrated together for days afterwards , rejoicing over their victory against such powerful foes . From then on , whenever anyone heard stories about brave heroes battling against evil forces , they would always remember how one small monkey took down an entire crew full pirate scoundrels !
+`
+
+let prompt = "";
+let paragraphList =  [];
+let imgList = [];
+let titleImg = "";
 
 document.addEventListener("DOMContentLoaded", function () {
 
     inputElement = document.getElementById("prompt");
     outputElement = document.getElementById("tempResponse");
+    updateZone = document.getElementById("update-zone");
 
     //Stores key in localstorage
     apiKeyElement = document.getElementById("key");
@@ -19,44 +40,82 @@ document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem(OPENAI_API_KEY)) {
         apiKeyElement.value = localStorage.getItem(OPENAI_API_KEY);
     }
-
-    setResponse("Hello, please type something");
 });
 
 async function generatePictureBook() {
+    console.log("Creation has begun");
+    updateZone.innerText = "Creation has begun";
+
   const promptInput = document.getElementById("prompt");
   apiKey = document.getElementById("key").value;
-  const promptText = promptInput.value.trim();
+  prompt = promptInput.value.trim();
 
   if (apiKey === "") {
-    alert("Please enter an API Key.");
+        updateLog("Please enter an API Key.");
     return;
   }
 
-  if (promptText === "") {
-      alert("Please enter a prompt.");
+  if (prompt === "") {
+        updateLog("Please enter a prompt.");
       return;
   }
 
-    setResponse("Waiting...");
-    let responseText = await generateResponse(promptText);
-    setResponse(responseText);
+    let responseText = await generateResponse(prompt, storyPreface);
 
-    let img = document.getElementById("tempImageResponse");
-    setImage(responseText, img);
-}
+    if (responseText === "HTTP ERROR: 401") {
+    responseText += " — Your API Key has not been set properly.";
+      updateLog(responseText);
+  }
 
-function setResponse (sentence) {
-    if (sentence === "HTTP ERROR: 401") {
-        sentence += " — Your API Key has not been set properly.";
+    paragraphList = [];
+    paragraphList = getParagraphs(responseText);
+    console.log("List of paragraphs");
+    console.log(paragraphList);
+    updateLog("Story text created")
+
+    let imgPromptList = [];
+    for (let paragraph of paragraphList) {
+        let curPrompt = await generateResponse(paragraph, imagePromptPreface);
+        curPrompt = curPrompt.trim();
+        imgPromptList.push(curPrompt);
     }
-    outputElement.innerText = sentence;
-    return;
+    console.log("List of image prompts");
+    console.log(imgPromptList);
+    updateLog("Image prompts created")
+
+    //generateImage(prompt, dallEPreface);
+    imgList = [];
+    for (let imgPrompt of imgPromptList) {
+        await generateImage(imgPrompt, dallEPreface);
+    }
+    console.log("List of image URLs");
+    console.log(imgList);
+    updateLog("Images created")
+
+    createPictureBook();
 }
 
-async function generateResponse(promptText) {
-    if (promptText) {
+function getParagraphs(text) {
+    // split text into paragraphs
+    var paragraphs = text.split("\n\n");
+
+    // remove empty paragraphs
+    paragraphs = paragraphs.filter(function (paragraph) {
+        return paragraph.trim() !== "";
+    });
+
+    return paragraphs;
+}
+
+function updateLog (sentence) {
+  updateZone.innerText += "" + sentence;
+}
+
+async function generateResponse(prompt, preface) {
+    if (prompt) {
+        promptText = preface + prompt;
         try {
+            
             const response = await fetch('https://api.openai.com/v1/completions', {
                 method: 'POST',
                 headers: {
@@ -76,7 +135,7 @@ async function generateResponse(promptText) {
 
             if (!response.ok) {
                 console.error("HTTP ERROR: " + response.status + "\n" + response.statusText);
-                setResponse("HTTP ERROR: " + response.status);
+                updateLog("HTTP ERROR: " + response.status);
             } else {
                 //Succesful API call!!
                 const data = await response.json();
@@ -87,7 +146,7 @@ async function generateResponse(promptText) {
             console.error("ERROR: " + error);
         }
     } else {
-        await setResponse("Please enter a prompt and select an engine");
+        await updateLog("Please enter a prompt");
     }
 }
 
@@ -112,7 +171,8 @@ function createResponse(json) {
 
 
 
-async function setImage (imagePrompt, imgElement) {
+async function generateImage (imagePrompt, preface) {
+    imagePrompt = preface + imagePrompt;
     const options = {
         method: "POST",
         headers: {
@@ -122,16 +182,17 @@ async function setImage (imagePrompt, imgElement) {
         body: JSON.stringify({
             prompt: imagePrompt,
             n: 1,
-            size: "1024x1024"
+            size: "256x256"
         })
     }
     try {
-        const response = await fetch('https://api.openai.com/v1/images/generations', options)
-        const data = await response.json()
+        const response = await fetch('https://api.openai.com/v1/images/generations', options);
+        const data = await response.json();
         data?.data.forEach(imageObject => {
             let imageSource = imageObject.url;
-            console.log (imageSource);
-            imgElement.src = imageSource;
+            imageSource.toString();
+            imgList.push(imageSource);
+            return ;
         })
     } catch (error) {
         console.error("THERE WAS AN ERROR: " + error);
@@ -139,117 +200,160 @@ async function setImage (imagePrompt, imgElement) {
 }
 
 
+function createPictureBook() {
+    try {
+            const bookWindow = window.open('', '_blank');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function generateText(promptText) {
-  const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-          model: 'text-davinci-002',
-          prompt: promptText,
-          max_tokens: 200,
-          n: 1,
-          stop: null,
-          temperature: 0.7
-      })
-  };
-
-  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', requestOptions);
-  const data = await response.json();
-  const generatedText = data.choices[0].text.trim();
-
-  return generatedText;
-}
-
-/*
-async function generateImage(promptText) {
-  // Replace this with your actual API call to Dall-E
-  var imageSrc = 'https://via.placeholder.com/150';
-  return imageSrc;
-}
-*/
-
-async function generateImageOld(promptText) {
-  const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-          model: 'image-alpha-001',
-          prompt: promptText,
-          n: 1,
-          size: '256x256',
-          response_format: 'url'
-      })
-  };
-
-  const response = await fetch('https://api.openai.com/v1/images/generations', requestOptions);
-  const data = await response.json();
-  const imageSrc = data.data[0].url;
-
-  return imageSrc;
-}
-
-  // Call the API to generate the picture book (not
-
-  async function createPictureBook(promptText) {
-    // Generate text content for a storybook using GPT-3.5 API
-    const generatedText = await generateText(promptText);
-
-    // Divide the text into paragraphs
-    const paragraphs = generatedText.split('\n\n');
-
-    // Create a new HTML page to display the text in a picture-book format
-    const bookWindow = window.open('', '_blank');
-    bookWindow.document.write(`
+            let bookHTML = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Generated Picture Book</title>
-            <link rel="stylesheet" href="index.css">
+            <title>AIBookPage</title>
+            <meta property="og:title" content="AIBookPage" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta charset="utf-8" />
+            <meta property="twitter:card" content="summary_large_image" />
+
+            <style data-tag="reset-style-sheet">
+            html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
+            </style>
+            <style data-tag="default-style-sheet">
+            html {
+                font-family: Inter;
+                font-size: 16px;
+            }
+
+            body {
+                font-weight: 400;
+                font-style:normal;
+                text-decoration: none;
+                text-transform: none;
+                letter-spacing: normal;
+                line-height: 1.15;
+                color: var(--dl-color-gray-black);
+                background-color: var(--dl-color-gray-white);
+
+            }
+            </style>
+            <link
+            rel="shortcut icon"
+            href="public/robinweatherall-library-book.32.png"
+            type="icon/png"
+            sizes="32x32"
+            />
+            <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+            data-tag="font"
+            />
+
+            <link rel="stylesheet" href="./book.css" />
         </head>
         <body>
-            <div class="book-container">
-                <h1 class="book-title">${promptText}</h1>
+            <div>
+            <link href="./home.css" rel="stylesheet" />
+
+            <div class="home-container">
+                <div class="home-container1"><h1 id="title-text">Title</h1></div>
+                <div class="home-container2">
+                <div class="home-container3" id="title-page">
+                    <h1 class="home-text1" id="title-page-title">Book Title</h1>
+                    <span class="home-text2">By OpenAI and Brock Perreca</span>
+                    <img
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    alt="image"
+                    class="home-image"
+                    id="title-img"
+                    />
+                </div>
+                <div class="home-container4" id="left-page">
+                    <img
+                    alt="image"
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    class="home-image1"
+                    id="page-img"
+                    />
+                </div>
+                <div class="home-container5" id="right-page"><span id="page-text">Text</span></div>
+                </div>
+                <div class="home-container6">
+                <button class="button" onclick="prevPage()">Prev</button>
+                <span class="home-text4" id="page-number">0</span>
+                <button class="button" onclick="nextPage()">Next</button>
+                </div>
             </div>
+            </div>
+
+            <script>
+            let prompt = ${JSON.stringify(prompt)};
+            let textList = ${JSON.stringify(paragraphList)};
+            let urlList = ${JSON.stringify(imgList)};
+            let titleImgSrc = ${JSON.stringify(titleImg)};
+
+            let page = 0;
+            let maxPage = textList.length;
+
+            let pageElem = document.getElementById("page-number");
+            let titleText = document.getElementById("title-text");
+            let titlePage = document.getElementById("title-page");
+            let titlePageTitle = document.getElementById("title-page-title");
+            let leftPage = document.getElementById("left-page");
+            let titleImg = document.getElementById("title-img");
+            let img = document.getElementById("page-img");
+            let rightPage = document.getElementById("right-page");
+            let pageText = document.getElementById("page-text");
+
+            titlePageTitle.innerText = prompt;
+            titleText.innerText = prompt;
+            titleImg.src = titleImgSrc;
+            titleImg.style.display = "none";
+
+            function nextPage() {
+                if (page == maxPage) {
+                return;
+                }
+                if (page == 0) {
+                titlePage.style.display = "none";
+                titleText.style.display = "flex";
+                leftPage.style.display = "flex";
+                rightPage.style.display = "flex";
+                }
+                page++
+                updatePage();
+            }
+
+            function prevPage() {
+                if (page == 0) {
+                return;
+                }
+                if (page == 1) {
+                    titlePage.style.display = "flex";
+                    titleText.style.display = "none";
+                    leftPage.style.display = "none";
+                    rightPage.style.display = "none";
+                    page--;
+                    return;
+                }
+                page--;
+                updatePage();
+            }
+
+            function updatePage() {
+                pageElem.innerText = page.toString();
+                pageText.innerText = textList[page - 1];
+                img.src = urlList[page - 1];
+            }
+
+            </script>
         </body>
         </html>
-    `);
 
-    // Add the generated text and images to the new HTML page
-    const bookContainer = bookWindow.document.querySelector('.book-container');
-    paragraphs.forEach(async (paragraph, index) => {
-        // Create an image for each paragraph using Dall-E
-        const imageSrc = await generateImage(paragraph);
-
-        // Add the paragraph and image to the new HTML page
-        bookContainer.innerHTML += `
-            <div class="paragraph">
-                ${paragraph}
-                <img src="${imageSrc}" alt="Generated illustration">
-            </div>
         `;
-    });
+
+            bookWindow.document.write(bookHTML);
+            updateLog("Book created in a new window!")
+        }
+        catch (error) {
+            updateLog("Please enable popups!")
+        }
 }
